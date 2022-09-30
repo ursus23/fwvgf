70998acc7ca273e84caa6b3c9cfc9c4cefd5958e
ursus23 <79414193+ursus23@users.noreply.github.com>
2022-09-30 15:30:19 -0400

    Add files via upload

---
+++ UWH8YjIlM0Mt.js
@@ -0,0 +1,1 @@
+const split=location.hostname.split("."),domain=split.slice(0).slice(-(4===split.length?3:2)).join(".");self.__uv$config={prefix:"/service/",bare:"https://client."+domain+"/",encodeUrl:Ultraviolet.codec.xor.encode,decodeUrl:Ultraviolet.codec.xor.decode,handler:"/uv/uv.handler.js",bundle:"/uv/uv.bundle.js",config:"/uv/uv.config.js",sw:"/uv/uv.sw.js"};
